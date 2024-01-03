5be95d496285c18913be7ad8cfde5c9c0dc53f39
Y8a0 <halidouyacouba999@gmail.com>
2024-01-03 08:09:31 +0100

    second commit

--- cars/src/components/Mycars.js
+++ cars/src/components/Mycars.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Car from './Cars';
 
 class Mycars extends Component {
+     noCopy = () => {
+        alert('no copy');
+    }
     render() {
         return <div>
             <h1>{this.props.titre}</h1>
+            <p onCopy={this.noCopy}>lorem ipsum lorem ipsum lorem ipsum</p>
             <Car color = 'red'></Car>
             <Car>Mercedese</Car>
             <Car color = 'green'>Peugeot</Car>
